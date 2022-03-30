0e07a69086af8614e5ed8ade49311f5e7950e487
OzerBey <yasinozeriletisim@gmail.com>
2022-03-30 18:02:10 +0300

    Update pom.xml and complete user entity

--- auction-shortened-url/src/main/java/com/ozer/auctionshortenedurl/entities/concretes/User.java
+++ auction-shortened-url/src/main/java/com/ozer/auctionshortenedurl/entities/concretes/User.java
@@ -1,5 +1,35 @@
 package com.ozer.auctionshortenedurl.entities.concretes;
 
-public class User {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+import com.sun.istack.NotNull;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id") // names must be same with in the Database
+	@NotNull
+	private int id;
+
+	@NotNull
+	@Column(name = "email")
+	private String email;
+
+	@NotNull
+	@Column(name = "password")
+	private String password;
 }
