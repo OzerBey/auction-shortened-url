0ba8e1f528f0d9c54045a6274f3803e7756fd0ae
OzerBey <yasinozeriletisim@gmail.com>
2022-03-30 18:47:37 +0300

    Create user controller

---
+++ auction-shortened-url/src/main/java/com/ozer/auctionshortenedurl/api/controllers/UsersController.java
@@ -0,0 +1,25 @@
+package com.ozer.auctionshortenedurl.api.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ozer.auctionshortenedurl.business.abstracts.UserService;
+import com.ozer.auctionshortenedurl.entities.concretes.User;
+
+@RestController
+@RequestMapping("/user")
+public class UsersController {
+	private UserService userService;
+
+	public UsersController(UserService userService) {
+		super();
+		this.userService = userService;
+	}
+
+	public List<User> getAll() {
+		return this.userService.getAll();
+	}
+
+}
