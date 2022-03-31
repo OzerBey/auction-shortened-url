cfcae19a547d6cc2f12804d1a8ca2e305c21566f
OzerBey <yasinozeriletisim@gmail.com>
2022-03-31 13:47:55 +0300

    Create a Url entity

---
+++ auction-shortened-url/src/main/java/com/ozer/auctionshortenedurl/entities/concretes/Url.java
@@ -0,0 +1,39 @@
+package com.ozer.auctionshortenedurl.entities.concretes;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "url")
+public class Url {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "url_id")
+	private int urlId;
+
+	@Column(name = "original_url")
+	private String originalUrl;
+
+	@Column(name = "short_url")
+	private String shortUrl;
+
+	@Column(name = "creation_date")
+	private LocalDateTime creationDate;
+
+	@Column(name = "expiration_date")
+	private LocalDateTime expirationDate;
+}
